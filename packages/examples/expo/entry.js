5eb057be42340528f3e0a00d1ebab4460bbba01a
Niklas Mischkulnig <4586894+mischnic@users.noreply.github.com>
2021-08-18 21:53:48 +0200

    f

--- packages/examples/expo/entry.js
+++ packages/examples/expo/entry.js
@@ -1,3 +1,9 @@
+// https://github.com/parcel-bundler/parcel/issues/6732
+import 'react-native/Libraries/polyfills/console.js';
+import 'react-native/Libraries/polyfills/error-guard.js';
+import 'react-native/Libraries/polyfills/Object.es7.js';
+import 'react-native/Libraries/Core/InitializeCore.js';
+
 import registerRootComponent from 'expo/build/launch/registerRootComponent';
 
 import App from './App';
--- packages/runtimes/react-native/src/ReactNativeRuntime.js
+++ packages/runtimes/react-native/src/ReactNativeRuntime.js
@@ -13,38 +13,10 @@ import 'react-native/Libraries/Core/InitializeCore.js';
 
 export default (new Runtime({
   async apply({bundle, options}) {
-    if (
-      bundle.type !== 'js' ||
-      !options.hmrOptions ||
-      !bundle.env.isBrowser() ||
-      bundle.env.isWorker() ||
-      bundle.env.isWorklet() ||
-      options.mode !== 'development' ||
-      bundle.env.sourceType !== 'module'
-    ) {
-      return;
-    }
-
-    let entries = bundle.getEntryAssets();
-    for (let entry of entries) {
-      // TODO: do this in loadConfig - but it doesn't have access to the bundle...
-      let pkg = await loadConfig(
-        options.inputFS,
-        entry.filePath,
-        ['package.json'],
-        options.projectRoot,
-      );
-      if (
-        pkg?.config?.dependencies?.react ||
-        pkg?.config?.devDependencies?.react ||
-        pkg?.config?.peerDependencies?.react
-      ) {
-        return {
-          filePath: path.join(options.projectRoot, 'index'),
-          code: CODE,
-          isEntry: true,
-        };
-      }
-    }
+    // return {
+    //   filePath: path.join(options.projectRoot, 'index'),
+    //   code: CODE,
+    //   isEntry: true,
+    // };
   },
 }): Runtime);
